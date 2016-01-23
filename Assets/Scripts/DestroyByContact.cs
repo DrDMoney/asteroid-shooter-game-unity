b4144b59cdfff4ffb9d36261b30eabd2de7b2d77
Daniel Mooney <the.d.money@gmail.com>
2016-01-23 02:03:05 -0500

    Initial Game Version

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public GameObject asteroid;
+	public int scoreValue;
+	private GameController gameController;
+	private float chunksScaleX;
+
+	public float chunks;
+	public float variance;
+
+	void Start (){
+	
+		//Finds instance of gamecontoller object
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			//gets the gamecontoller script
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+
+		if (gameController == null) {
+			Debug.Log("Cannot find 'GameController' script");
+		}
+
+	}
+
+
+	void OnTriggerEnter (Collider other)
+    {
+		//Don't want to distroy asteroids inside the boundy and asteroids that have nodamage do no damnage to player.  
+		if (other.tag == "Boundry" || (gameObject.tag == "NoDamage" && other.tag != "Bolt"))
+        {
+            return;
+        }
+
+		var explosionIntance = Instantiate(explosion, transform.position, transform.rotation) as GameObject;
+		explosionIntance.transform.localScale = gameObject.transform.localScale;
+
+		if (gameObject.transform.localScale.x > 1.0) {
+			makeAsteroidChunks (chunks);
+
+		}
+
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			//gameController.PlayerDeath ();
+		}
+		gameController.AddScore(scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+    }
+
+
+
+
+	void makeAsteroidChunks(float chunks){
+		for (int i = 0; i < chunks; i = i + 2) {
+			//random size of chunks based off current gameobject size
+			chunksScaleX = gameObject.transform.localScale.x/(chunks/Random.Range(1.0f,2.5f));
+			var asteroidIntance = Instantiate (asteroid, new Vector3 (transform.position.x + Random.Range (-variance, variance), 0.0f, transform.position.z + Random.Range (-variance, variance)), new Quaternion (Random.Range (-180, 180), Random.Range (-180, 180), Random.Range (-180, 180), Random.Range (-180, 180))) as GameObject;
+			asteroidIntance.transform.localScale = new Vector3 (chunksScaleX, chunksScaleX, chunksScaleX);
+			if (asteroidIntance.transform.localScale.x < 0.5f) {
+				asteroidIntance.tag ="NoDamage";
+			}
+
+		}
+	
+	
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float startWait;
+	public float spawnWait;
+	public float waveWait;
+
+	private GUIText scoreText;
+	private GUIText gameOverText;
+	private GUIText restartText;
+	private int score;
+
+	void Start()
+	{
+		//Spawning waves
+		StartCoroutine(SpawnWaves ());
+
+
+		//Scoring
+		//Gets the gameobject instance with the tag "score text"
+		score = 0;
+		GameObject scoreTextObject = GameObject.FindGameObjectWithTag ("Score Text");
+		if (scoreTextObject != null)
+		{
+			scoreText = scoreTextObject.GetComponent<GUIText> ();
+		}
+
+		if (scoreText == null) {
+			Debug.Log("Cannot find scoring text");
+		}
+		UpdateScore ();
+
+
+		//Game over
+		GameObject gameOverObject = GameObject.FindGameObjectWithTag ("Game Over");
+		if (gameOverObject != null)
+		{
+			gameOverText = gameOverObject.GetComponent<GUIText> ();
+		}
+		if (gameOverText == null) {
+			Debug.Log("Cannot find game-over text");
+		}
+
+		//restart
+		GameObject restartObject = GameObject.FindGameObjectWithTag ("Restart");
+		if (restartObject != null)
+		{
+			restartText = restartObject.GetComponent<GUIText> ();
+		}
+		if (restartText == null) {
+			Debug.Log("Cannot find restart text");
+		}
+			
+		gameOverText.enabled = false;
+		restartText.enabled = false;
+		restartText.text = "";
+		gameOverText.text = "";
+	}
+
+	void Update(){
+		if (restartText.enabled) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			}
+		
+		}
+	}
+
+	//swawn hazzard
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true) 
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range(spawnValues.z,spawnValues.z + 1.0f));
+				Quaternion spawnRotation = Quaternion.identity;
+				var instanAsteroid = Instantiate(hazard, spawnPosition, spawnRotation) as GameObject;
+				float num= Random.Range (.3f, 2.5f);
+				instanAsteroid.transform.localScale = new Vector3 (num, num, num);
+				instanAsteroid.GetComponent<DestroyByContact> ().scoreValue = (int)(num * 10);
+				instanAsteroid.GetComponent<Rigidbody> ().mass = num;
+
+
+
+
+
+
+				yield return new WaitForSeconds (spawnWait);
+			}
+				
+			yield return new WaitForSeconds (waveWait);
+			if (gameOverText.enabled) {break;}
+
+		}
+	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score = score + newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore(){
+		scoreText.text = "Score: " + score;
+	}
+
+	public void PlayerDeath (){
+		gameOverText.text = "Game over";
+		gameOverText.enabled = true;
+		restartText.text = "Press 'R' to Restart";
+		restartText.enabled = true;
+
+	}
+}
